09647193eac061c1dc44a4b429f73b3292e7ed89
jin605 <jinddd3@gmail.com>
2026-02-12 17:07:02 +0900

    java upload

---
+++ src/practice/java/boj10989_1.java
@@ -0,0 +1,32 @@
+package practice.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj10989_1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] count = new int[10001];
+
+        for (int i = 0; i < n; i++){
+            count[Integer.parseInt(br.readLine())]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 10000; i++){
+            while (count[i] > 0){
+                sb.append(i).append("\n");
+                count[i]--;
+            }
+        }
+
+        System.out.println(sb);
+
+
+
+
+    }
+}
